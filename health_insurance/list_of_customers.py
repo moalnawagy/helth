1321c3574d216a5eac1746c5d8a0ac15fd50e95c
moalnawagy <76096332+moalnawagy@users.noreply.github.com>
2021-12-31 13:42:05 +0200

    Delete health_insurance directory

--- health_insurance/list_of_customers.py
+++
@@ -1,6 +0,0 @@
-from core.db.db import db
-
-def get_list_of_customers():
-    list_of_customers = db("SELECT * FROM Customers")
-    return(list_of_customers)
-
--- health_insurance/mark_claim.py
+++
@@ -1,7 +0,0 @@
-from core.db.db import db
-
-
-#if claim accepted it'll be true if not it will be false
-
-def mark_claim(true_or_false, IC_id):
-    db(f'''update insurance_claim set `status` = {true_or_false} where IC_id = {IC_id};''')
